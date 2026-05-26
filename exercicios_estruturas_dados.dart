4df258b7e9ee6335b28df5bfc750b91812567a7b
Pedro Henrique Ribeiro <phassumpcao246@gmail.com>
2026-05-26 08:33:51 -0300

    Add initial structure for data structures exercises

---
+++ exercicios_estruturas_dados.dart
@@ -0,0 +1,28 @@
+void main() {
+ 
+  // 01) 
+//   List<String> produtos = ["Arroz", "Feijão", "Macarrão", "Leite", "Café"];
+  
+//   produtos.add("Açucar");
+//   produtos.add("Farinha");
+  
+//   produtos.forEach((produto) {
+//     print(produto);
+//   });
+  
+//   for (int i = 0; i < produtos.length; i++) {
+//     print(produtos);
+//   } 
+ 
+//   produtos.remove("Arroz");
+//   produtos.removeAt(3);
+  
+//   print(produtos);
+  
+//   if (produtos == null) {
+//     print("Lista está vazia");
+//   }
+//   else {
+//     print("Lista não está vazia ");
+//   }
+}
