a803a19c165bb5c4135457b847265553d7f416c1
Pedro Henrique Ribeiro <phassumpcao246@gmail.com>
2026-06-01 21:42:41 -0300

    Create exercicio_heranca_POO.dart

---
+++ exercicio_heranca_POO.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+
+// Classe abstrata
+abstract class Funcionario {
+  String nome;
+  double salario;
+
+  Funcionario(this.nome, this.salario);
+
+  void exibirDados();
+
+  void trabalhar();
+}
+
+// Classe Gerente
+class Gerente extends Funcionario {
+  Gerente(String nome, double salario) : super(nome, salario);
+
+  @override
+  void exibirDados() {
+    print('Gerente: $nome');
+    print('Salário: R\$ $salario');
+  }
+
+  @override
+  void trabalhar() {
+    print('$nome está gerenciando a equipe.');
+  }
+}
+
+// Classe Caixa
+class Caixa extends Funcionario {
+  Caixa(String nome, double salario) : super(nome, salario);
+
+  @override
+  void exibirDados() {
+    print('Caixa: $nome');
+    print('Salário: R\$ $salario');
+  }
+
+  @override
+  void trabalhar() {
+    print('$nome está atendendo no caixa.');
+  }
+}
+
+// Classe Atendente
+class Atendente extends Funcionario {
+  Atendente(String nome, double salario) : super(nome, salario);
+
+  @override
+  void exibirDados() {
+    print('Atendente: $nome');
+    print('Salário: R\$ $salario');
+  }
+
+  @override
+  void trabalhar() {
+    print('$nome está atendendo os clientes.');
+  }
+}
+
+// Classe depósito para armazenar os funcionários
+class Deposito {
+  List<Funcionario> funcionarios = [];
+
+  void adicionarFuncionario(Funcionario funcionario) {
+    funcionarios.add(funcionario);
+    print('Funcionário adicionado com sucesso!');
+  }
+
+  void listarFuncionarios() {
+    if (funcionarios.isEmpty) {
+      print('Nenhum funcionário cadastrado.');
+    } else {
+      print('\n--- Lista de Funcionários ---');
+      for (var funcionario in funcionarios) {
+        funcionario.exibirDados();
+        funcionario.trabalhar();
+        print('----------------------------');
+      }
+    }
+  }
+}
+
+// Função principal
+void main() {
+  Deposito deposito = Deposito();
+
+  int opcao = 0;
+
+  do {
+    print('\n===== MENU =====');
+    print('1 - Adicionar Gerente');
+    print('2 - Adicionar Caixa');
+    print('3 - Adicionar Atendente');
+    print('4 - Listar Funcionários');
+    print('5 - Sair');
+    stdout.write('Escolha uma opção: ');
+
+    opcao = int.parse(stdin.readLineSync()!);
+
+    switch (opcao) {
+      case 1:
+        stdout.write('Digite o nome do gerente: ');
+        String nome = stdin.readLineSync()!;
+
+        stdout.write('Digite o salário do gerente: ');
+        double salario = double.parse(stdin.readLineSync()!);
+
+        deposito.adicionarFuncionario(Gerente(nome, salario));
+        break;
+
+      case 2:
+        stdout.write('Digite o nome do caixa: ');
+        String nome = stdin.readLineSync()!;
+
+        stdout.write('Digite o salário do caixa: ');
+        double salario = double.parse(stdin.readLineSync()!);
+
+        deposito.adicionarFuncionario(Caixa(nome, salario));
+        break;
+
+      case 3:
+        stdout.write('Digite o nome do atendente: ');
+        String nome = stdin.readLineSync()!;
+
+        stdout.write('Digite o salário do atendente: ');
+        double salario = double.parse(stdin.readLineSync()!);
+
+        deposito.adicionarFuncionario(Atendente(nome, salario));
+        break;
+
+      case 4:
+        deposito.listarFuncionarios();
+        break;
+
+      case 5:
+        print('Saindo do sistema...');
+        break;
+
+      default:
+        print('Opção inválida!');
+    }
+  } while (opcao != 5);
+}
